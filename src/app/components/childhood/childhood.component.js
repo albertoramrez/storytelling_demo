704eb40d37ae322f424b744c92ae1addf49fed76
Blanca Azucena López Garduño <blanca.lopez@data4.mx>
2017-10-12 11:22:54 -0500

    add changing color feature with css3 transition

--- src/app/components/childhood/childhood.component.js
+++ src/app/components/childhood/childhood.component.js
@@ -30,23 +30,33 @@
         switch($storytelling.currentSlide) {
           case 0:
             vm.dynamicText = '¡Ahora estoy arriba, mira esto!';
+
             d3.select($element[0]).select('h1').style('display', 'block');
+
             d3.select($element[0]).select('.circle').style('display', 'none');
 
             break;
           case 1:
             d3.select($element[0]).select('h1').style('display', 'none');
+
             d3.select($element[0]).select('.circle').style('display', 'block');
+
             $storytelling.animateElem('#childhood .circle', 'fadeIn');
-            d3.select($element[0]).select('.circle').style('width', '5vh')            
-              .style('height', '5vh');
+
+            d3.select($element[0]).select('.circle')
+              .style('width', '5vh')            
+              .style('height', '5vh')
+              .style('background-color', '#e9d208');
             break;
           case 2:
             vm.dynamicText = '¡También puede crecer!';
-            d3.select($element[0]).select('h1').style('display', 'block');     
+
+            d3.select($element[0]).select('h1').style('display', 'block');   
+
             d3.select($element[0]).select('.circle').style('display', 'block')
               .style('width', '20vh')            
-              .style('height', '20vh');
+              .style('height', '20vh')
+              .style('background-color', '#ba1a3a')
 
             break;
         }
